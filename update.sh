bcba1880c665de1b182e927919a297abd1483dde
Deniss Muhļa <deniss.muhla@if.lv>
2025-01-17 14:05:42 +0200

    Fix update script

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR" || exit 1
+
 docker stop passivbot
 git pull
 docker compose -f "docker-compose.yml" up -d --build
