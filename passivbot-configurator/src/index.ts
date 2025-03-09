bb77ac7a23d95b9c68f46a2b680bdf8e67224345
Deniss Muhļa <deniss.muhla@gmail.com>
2025-03-09 14:56:59 +0200

    config

--- passivbot-configurator/src/index.ts
+++ passivbot-configurator/src/index.ts
@@ -3,19 +3,27 @@ import { PATHS } from "./utils";
 import { Config } from "./config";
 import { writeJSON } from "fs-extra";
 
-const version = "2.1.1";
-const configPath = path.resolve(PATHS.CONFIGS, "bybit");
-const configSymbols = ["PEPE", "HYPE", "USUAL"]; //WIF PEPE HYPE PNUT USUAL HBAR
+// TODO: "ADA"
+// TODO: "SUI" ?+ "ENA"
+const version = "2.1.3";
+const configPath = path.resolve(PATHS.CONFIGS, `bybit-${version}`);
+const configSymbols = ["PEPE", "HYPE", "USUAL"];
 const dateRange = 30;
 const nPositionsMin = 2.5;
 const nPositionsMax = 3.4;
-const templateConfigFilePath = path.resolve(PATHS.CONFIGS, "templates/bybit.json");
+const templateConfigFilePath = path.resolve(PATHS.CONFIGS, "templates/bybit-1.0.0.json");
 
 (async () => {
     const config = Config.createFromTemplateConfigFile("config", configPath, templateConfigFilePath);
     config.setSymbols(configSymbols);
     config.setOptimizationBoundsNPositions(nPositionsMin, nPositionsMax);
     config.setDateRange(dateRange);
+
+    if (config.configFile.optimize) {
+        config.configFile.optimize.bounds.long_total_wallet_exposure_limit = [0.25, 0.75];
+        config.configFile.optimize.bounds.short_total_wallet_exposure_limit = [0.25, 0.75];
+    }
+
     config.save();
     await config.optimize();
     config.applyOptimizedConfig();
