83a2e5828a8a422156521006cf23d2e2cb7a8f0f
Deniss Muhļa <deniss.muhla@gmail.com>
2025-04-26 07:57:09 +0300

    config

--- passivbot-configurator/src/index.ts
+++ passivbot-configurator/src/index.ts
@@ -8,13 +8,13 @@ import { writeJSON } from "fs-extra";
 // OTHERS: "USUAL", "FARTCOIN", "ADA", "RARE", "SUI", "ENA", "AAVE", "WIF", "OP", "LINK"
 // TODO: "ADA", "RARE"
 // TODO: "SUI" ?+ "ENA" ?+ "AAVE"
-const version = "3.2.2";
+const version = "HYPE-3.3.0";
 const configPath = path.resolve(PATHS.CONFIGS, `bybit-${version}`);
 //const optimizationPrimarySymbols: string[] = ["BTC"];
 const configSymbols: string[] = ["HYPE"];
 const nPositionsMin = 1;
 const nPositionsMax = 1;
-const totalWalletExposureLimit: [number, number] = [1.25, 2];
+const totalWalletExposureLimit: [number, number] = [1.25, 2.25];
 const templateConfigFilePath = path.resolve(PATHS.CONFIGS, `templates/bybit-${version}.json`);
 
 const optimize = async (dateRange: number) => {
@@ -29,6 +29,20 @@ const optimize = async (dateRange: number) => {
     }
 
     config.save();
+
+    await writeJSON(
+        path.resolve(configPath, "meta.json"),
+        {
+            version,
+            //optimizationPrimarySymbols,
+            symbols: configSymbols,
+            dateRange,
+            nPositions: [nPositionsMin, nPositionsMax],
+            templateConfigFilePath,
+        },
+        { spaces: 4 }
+    );
+
     await config.optimize();
     config.applyOptimizedConfig();
 
@@ -52,19 +66,6 @@ const optimize = async (dateRange: number) => {
         config.linkSymbolConfig(symbol);
         config.save();
     }
-
-    await writeJSON(
-        path.resolve(configPath, "meta.json"),
-        {
-            version,
-            //optimizationPrimarySymbols,
-            symbols: configSymbols,
-            dateRange,
-            nPositions: [nPositionsMin, nPositionsMax],
-            templateConfigFilePath,
-        },
-        { spaces: 4 }
-    );
 };
 
 const optimizeSymbols = async (dateRange: number) => {
@@ -72,22 +73,6 @@ const optimizeSymbols = async (dateRange: number) => {
     config.setSymbols(configSymbols);
     config.setDateRange(dateRange);
     config.save();
-
-    for (const symbol of configSymbols) {
-        const symbolConfig = Config.createFromTemplateConfigFile(symbol, configPath, templateConfigFilePath);
-        symbolConfig.setSymbols([symbol]);
-        symbolConfig.setDateRange(dateRange * 2);
-        symbolConfig.setOptimizationGlobalBounds(config.configFile);
-        symbolConfig.save();
-        await symbolConfig.optimize();
-        symbolConfig.applyOptimizedConfig();
-
-        // Save symbol config
-        symbolConfig.save();
-        await symbolConfig.backtest();
-        config.linkSymbolConfig(symbol);
-        config.save();
-    }
 
     await writeJSON(
         path.resolve(configPath, "meta.json"),
@@ -102,6 +87,23 @@ const optimizeSymbols = async (dateRange: number) => {
         },
         { spaces: 4 }
     );
+
+    for (const symbol of configSymbols) {
+        const symbolConfig = Config.createFromTemplateConfigFile(symbol, configPath, templateConfigFilePath);
+        symbolConfig.setSymbols([symbol]);
+        symbolConfig.setDateRange(dateRange * 2);
+        symbolConfig.setOptimizationGlobalBounds(config.configFile);
+        symbolConfig.save();
+
+        await symbolConfig.optimize();
+        symbolConfig.applyOptimizedConfig();
+
+        // Save symbol config
+        symbolConfig.save();
+        await symbolConfig.backtest();
+        config.linkSymbolConfig(symbol);
+        config.save();
+    }
 };
 
 const optimizeSingle = async (dateRange: number) => {
@@ -116,13 +118,6 @@ const optimizeSingle = async (dateRange: number) => {
     }
 
     config.save();
-    await config.optimize();
-    config.applyOptimizedConfig();
-
-    // Save config
-    config.setSymbols(configSymbols);
-    config.save();
-    await config.backtest();
 
     await writeJSON(
         path.resolve(configPath, "meta.json"),
@@ -137,6 +132,14 @@ const optimizeSingle = async (dateRange: number) => {
         },
         { spaces: 4 }
     );
+
+    await config.optimize();
+    config.applyOptimizedConfig();
+
+    // Save config
+    config.setSymbols(configSymbols);
+    config.save();
+    await config.backtest();
 };
 
 const backtest = async (dateRange: number) => {
@@ -164,10 +167,10 @@ const backtestSingle = async (dateRange: number) => {
 };
 
 (async () => {
-    await optimizeSingle(7 * 4);
+    //await optimizeSingle(7 * 4);
     //await optimize(7 * 2);
     //await optimizeSymbols(7 * 2);
-    await backtestSingle(7 * 12);
+    await backtestSingle(7 * 4 * 12);
     //await backtest(7 * 6);
     //await backtest(7 * 12);
 })();
